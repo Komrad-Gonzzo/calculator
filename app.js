dd005ec478b2aa83214b60212a19073c5e44b64b
Komrad-Gonzzo <crst.eliseev@gmail.com>
2022-11-29 00:44:49 +0300

    add screen display

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ let b = '';//secont number
 let sign = '';//знак операции
 let finish = false;
 
-const digit = ['0','1','2','3','4','5','6','7','8','9','.',]
+const digit = ['0','1','2','3','4','5','6','7','8','9','.',];
+const action = ['-','+','X','/'];
+
+//экран калькулятора
+const out = document.querySelektor('.calk-screen p');
